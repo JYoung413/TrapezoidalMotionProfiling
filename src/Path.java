83f9b57b5d53dfabc15655a75e59141bfc31432e
Josh <joshayoung125@gmail.com>
2017-10-24 16:10:33 -0400

    Initial commit

---
+++ src/Path.java
@@ -0,0 +1,28 @@
+public class Path {
+    private Trajectory.Pair trajectories;
+    private String name;
+
+    public Path(String name, Trajectory.Pair trajectories) {
+        this.name = name;
+        this.trajectories = trajectories;
+    }
+
+    public Path() {
+        //left blank intentionally
+    }
+
+    public Trajectory getLeftTrajectory() {
+        return trajectories.left;
+    }
+
+    public Trajectory getRightTrajectory() {
+        return trajectories.right;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+
+}
